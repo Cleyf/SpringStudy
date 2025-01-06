69a56d0919469c260741922b236298397e599895
Cleyfson Angelo <cleyfson12@outlook.com>
2025-01-06 18:42:01 -0300

    Atualizar por id

--- produtosapi/src/main/java/io/github/cleyf/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/io/github/cleyf/produtosapi/controller/ProdutoController.java
@@ -38,6 +38,11 @@ public class ProdutoController {
     @DeleteMapping("{id}")
     public void deletar(@PathVariable("id") String id) {
         produtoRepository.deleteById(id);
+    }
 
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id") String id, @RequestBody Produto produto) {
+        produto.setId(id);
+        produtoRepository.save(produto);
     }
 }
