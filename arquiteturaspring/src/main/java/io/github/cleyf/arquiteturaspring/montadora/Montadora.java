c4cd779c7159f9330718ffa49f0a5dfefc2297db
Cleyfson Angelo <cleyfson12@outlook.com>
2025-02-11 20:23:56 -0300

    criando modelo do contexto

---
+++ arquiteturaspring/src/main/java/io/github/cleyf/arquiteturaspring/montadora/Montadora.java
@@ -0,0 +1,6 @@
+package io.github.cleyf.arquiteturaspring.montadora;
+
+public enum Montadora {
+    HONDA,
+    TOYOTA
+}
---
+++ arquiteturaspring/src/main/java/io/github/cleyf/arquiteturaspring/montadora/TipoMotor.java
@@ -0,0 +1,7 @@
+package io.github.cleyf.arquiteturaspring.montadora;
+
+public enum TipoMotor {
+    ASPIRADO,
+    TURBO,
+    ELETRICO
+}
