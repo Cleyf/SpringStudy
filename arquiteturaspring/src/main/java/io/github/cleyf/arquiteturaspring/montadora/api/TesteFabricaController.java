3a65f66e7584bb116a107a942c7ee24cc526feb6
Cleyfson Angelo <cleyfson12@outlook.com>
2025-02-12 20:34:03 -0300

    Utilizando qualifiers

--- arquiteturaspring/src/main/java/io/github/cleyf/arquiteturaspring/montadora/api/TesteFabricaController.java
+++ arquiteturaspring/src/main/java/io/github/cleyf/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -2,13 +2,17 @@ package io.github.cleyf.arquiteturaspring.montadora.api;
 
 import io.github.cleyf.arquiteturaspring.montadora.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/carros")
 public class TesteFabricaController {
     @Autowired
+    @Qualifier("motorTurbo")
     private Motor motor;
 
     @PostMapping
