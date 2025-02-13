557601628cefdb9122f70a19d824aae2c43ffe0d
Cleyfson Angelo <cleyfson12@outlook.com>
2025-02-13 18:59:27 -0300

    Codificando as camadas da arquitetura completa

---
+++ arquiteturaspring/src/main/java/io/github/cleyf/arquiteturaspring/todos/TodoService.java
@@ -0,0 +1,16 @@
+package io.github.cleyf.arquiteturaspring.todos;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    private TodoRepository repository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.repository = todoRepository;
+    }
+    public TodoEntity salvar(TodoEntity novoTodo) {
+        return repository.save(novoTodo);
+    }
+}
